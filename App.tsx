b81d7b128f2ab4f2cbfdd9d9750eb7f61be485f3
Donghwan Lee <dhwanyi@gmail.com>
2026-06-28 16:10:48 -0700

    Milestone 1 complete: 9x9 Sudoku grid with 3x3 borders

--- App.tsx
+++ App.tsx
@@ -1,11 +1,41 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const grid_layout = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+  // our memory slot. It will track { row: X, col: Y}
+  const [selectedCell, setSelectedCell] = useState<{row:number, col:number} | null>(null)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+        <View style={styles.board}>
+            {grid_layout.map((row) => (
+        <View key={row} style={styles.row}>
+            {grid_layout.map((col) => {
+                // a 3x3 boundary line
+                const isThickBottom = row === 2 || row === 5;
+                const isThickRight = col === 2 || col === 5;
+                
+                return (
+                    <View 
+                        key={col} 
+                        style={[
+                            styles.cell,
+                            isThickBottom && styles.thickBottom,
+                            isThickRight && styles.thickRight,
+                        ]}
+                    >
+                        <Text style={styles.cellText}>5</Text>
+                    </View>  
+                );
+            })}
+        </View>
+      ))}
+      {/* <Text>Open up App.tsx to start working on your app!</Text>*/}
       <StatusBar style="auto" />
+        </View>
     </View>
   );
 }
@@ -16,5 +46,32 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  board: {
+    borderWidth: 2,
+    borderColor: '#000',
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  cell: {
+    width: 40,
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  cellText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  thickBottom: {
+    borderBottomWidth: 2,
+    borderBottomColor: '#000',
+  },
+  thickRight: {
+    borderRightWidth: 2,
+    borderRightColor: '#000',
   },
 });
